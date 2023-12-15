259150e331d18ebc3e18c4eda54010a704a352e0
Ubuntu <hanisten@Hanisten.53h43fzh542u3d5fbmcsftj20f.bx.internal.cloudapp.net>
2023-12-15 14:29:40 +0000

    Datenbank erstellt

---
+++ Backend/ContactModel.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+ 
+const ContactSchema = new Schema({
+    _id: {
+        type: Number,
+        required: true
+    },
+    Username: {
+        type: String,
+        required: [true, "Username is required"]
+    },
+    FirstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    LastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
+    Password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+}, { collection: 'Users' });
+ 
+const ContactModel = model('ContactModel', ContactSchema);
+ 
+export default ContactModel;
+ 
